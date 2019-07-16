845fa8819d135b765dc7a807b48c4559fdcc6205
Celal Cankaya <celalcankaya@outlook.com>
2019-07-16 20:06:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,13 @@
 # Make your shoe class here!
 class Shoe
-  attr_accessor :size, :brand, :color, :material, :condition, :cobble
+  attr_accessor :size, :brand, :color, :material, :condition
 
   def initialize(brand)
     @brand = brand
   end
 
+  def cobble
+    puts  "Your shoe is as good as new!"
+    @condition = "new"
+  end
 end
