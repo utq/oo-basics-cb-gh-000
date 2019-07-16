0e4852e853b9543b078f82675197ef40902bbedb
Celal Cankaya <celalcankaya@outlook.com>
2019-07-16 19:59:56 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :size, :brand, :genre
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
